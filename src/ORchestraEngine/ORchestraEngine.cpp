b3bc32db916c0b2e4219f6d26c307a422bdb4b4c
Christian Tronhjem <ctronhjem@gmail.com>
2025-11-01 23:00:19 +0100

    Set update back to 20ms and commments to make sure we don't skip a beat in the preprocess

--- src/ORchestraEngine/ORchestraEngine.cpp
+++ src/ORchestraEngine/ORchestraEngine.cpp
@@ -62,7 +62,7 @@ void ORchestraEngine::WorkerThreadLoop()
     {
         PreProcessSteps();
         // TODO: Do math for sleep based on BPM
-        std::this_thread::sleep_for(std::chrono::milliseconds(50));
+        std::this_thread::sleep_for(std::chrono::milliseconds(20));
     }
 }
 
