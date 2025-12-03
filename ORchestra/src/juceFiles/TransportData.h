62a02766ef8abc371c02657264fc6c977fb93573
Christian Tronhjem <ctronhjem@gmail.com>
2025-12-04 00:56:17 +0100

    Remove old data in TransportData

--- ORchestra/src/juceFiles/TransportData.h
+++ ORchestra/src/juceFiles/TransportData.h
@@ -10,10 +10,5 @@ namespace ORchestra
         int noteLengthInSamples = 0;
         int64_t timeInSamples = 0;
         bool isPlaying = false;
-
-        //    double time = 0.0;
-        //    double ppq = 0.0;
-        //    int bar = 0;
-        //    int beat = 0;
     };
 } // namespace ORchestra
